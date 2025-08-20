c09ea09296d39bdaff7c2768ac9dc3c1fa3a41ec
Sreejadusa <dusasreeja@gmail.com>
2025-08-20 16:24:27 +0530

    Add Bus Ticket Booking App

--- Django/travels/travels/settings.py
+++ Django/travels/travels/settings.py
@@ -72,8 +72,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:5174',
-    'http://localhost:5175',
-    'http://localhost:5176',
+    'https://bus-ticket-bookingapp.netlify.app',
 ]
 
 
